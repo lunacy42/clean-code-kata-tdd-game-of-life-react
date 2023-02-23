515aecce85281346e01882f312be5470dba5b404
Carolin Ivens <18504794+lunacy42@users.noreply.github.com>
2023-02-24 00:12:47 +0100

    removed content App.tsx and added first test for title (failing, because title not set yet)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText("Game of Life");
+  expect(title).toBeInTheDocument();
 });
+
