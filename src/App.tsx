46ffaf6287c0e1b69a177e24172c207d605f26e1
Carolin Ivens <18504794+lunacy42@users.noreply.github.com>
2023-02-24 00:40:08 +0100

    added board component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Board from './components/Board';
 
 function App() {
   return (
     <div className="App">
       <h1>Game of Life</h1>
+      <Board />
     </div>
   );
 }
---
+++ src/components/Board.tsx
@@ -0,0 +1,10 @@
+import './Board.css';
+
+const Board = () => {
+  return (
+    <div className="Board">
+    </div>
+  );
+}
+
+export default Board;
