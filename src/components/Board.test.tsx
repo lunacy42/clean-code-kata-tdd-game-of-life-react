023b6b6d912a6430a3b471a57b2e80056d108cbc
Carolin Ivens <18504794+lunacy42@users.noreply.github.com>
2023-02-26 18:02:14 +0100

    make test pass, added dead and living cells

--- src/components/Board.test.tsx
+++ src/components/Board.test.tsx
@@ -3,20 +3,20 @@ import Board from './Board';
 
 it('renders at least one cell', () => {
   render(<Board />);
-  const cells = screen.getAllByTestId('cell');
+  const cells = screen.getAllByTestId('cell', { exact: false });
   expect(cells[0]).toBeInTheDocument();
 });
 
 it('renders 1200 cells', () => {
   render(<Board />);
-  const cells = screen.getAllByTestId('cell');
+  const cells = screen.getAllByTestId('cell', { exact: false });
   expect(cells).toHaveLength(1200);
 });
 
 it('renders at least one living and at least one dead cell', () => {
   render(<Board />);
-  const livingCells = screen.getAllByTestId('livingCell');
-  const deadCells = screen.getAllByTestId('deadCell');
+  const livingCells = screen.getAllByTestId('living-cell');
+  const deadCells = screen.getAllByTestId('dead-cell');
   expect(livingCells[0]).toBeInTheDocument();
   expect(deadCells[0]).toBeInTheDocument();
 });
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -5,28 +5,34 @@ const Board = () => {
   const numRows = 20;
   const numCols = 60;
 
-  const getCells = () => {
-    let rows = [];
+  const getRandomCells = (filling: string) => {
+    const rows = [];
     for (let y = 1; y <= numRows; y++) {
-      let column = [];
+      const column = [];
       for (let x = 1; x <= numCols; x++) {
-        column.push(0);
+        column.push(filling === 'filled' ? Math.random() < 0.3 : 0);
       }
       rows.push(column);
     }
     return rows;
   };
 
-  const [cells, setCells] = useState(getCells());
+  const [cells, setCells] = useState(getRandomCells('filled'));
 
-  const getRows = (row: number[], i: number) => {
-    return row.map((cell: number, j: number) => {
-      return <div key={`${i}-${j}`} className="Cell" data-testid="cell"></div>;
+  const getRows = (row: (number | boolean)[], i: number) => {
+    return row.map((cell: number | boolean, j: number) => {
+      return (
+        <div
+          key={`${i}-${j}`}
+          className="Cell"
+          data-testid={cells[i][j] ? 'living-cell' : 'dead-cell'}
+          style={{ backgroundColor: cells[i][j] ? '#2da4ab' : '#fff' }}></div>
+      );
     });
   };
 
   const getColumns = () => {
-    return cells.map((row: number[], i: number) => {
+    return cells.map((row: (number | boolean)[], i: number) => {
       return (
         <div key={i} className="Row">
           {getRows(row, i)}
