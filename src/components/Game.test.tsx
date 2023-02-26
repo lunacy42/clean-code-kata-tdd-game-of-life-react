020865b996a3ed85a709237146574f0d493bd280
Carolin Ivens <18504794+lunacy42@users.noreply.github.com>
2023-02-26 22:35:04 +0100

    added tests for buttons, currently failing

--- src/components/Game.test.tsx
+++ src/components/Game.test.tsx
@@ -1,4 +1,4 @@
-import { render, renderHook, screen } from '@testing-library/react';
+import { fireEvent, render, renderHook, screen } from '@testing-library/react';
 import useInterval from '../hooks/useInterval';
 import { getNextCells } from '../utils/getNextCells';
 import Game from './Game';
@@ -228,10 +228,97 @@ it('calls useInterval-hook every 500ms', () => {
 
   renderHook(() => useInterval(callback, 500));
   render(<Game />);
+  const startButton = screen.getByText('Start');
+  fireEvent.click(startButton);
   jest.advanceTimersByTime(499);
   expect(callback).toHaveBeenCalledTimes(0);
   jest.advanceTimersByTime(1);
   expect(callback).toHaveBeenCalledTimes(1);
   jest.advanceTimersByTime(50000);
   expect(callback).toHaveBeenCalledTimes(101);
+});
+
+it('starts game after clicking start button', () => {
+  const callback: any = jest.fn();
+
+  jest.useFakeTimers();
+
+  renderHook(() => useInterval(callback, 500));
+  render(<Game />);
+  // no callbacks if game not started
+  jest.advanceTimersByTime(5000);
+  expect(callback).toHaveBeenCalledTimes(0);
+
+  // start game
+  const startButton = screen.getByText('Start');
+  fireEvent.click(startButton);
+
+  jest.advanceTimersByTime(499);
+  expect(callback).toHaveBeenCalledTimes(0);
+  jest.advanceTimersByTime(1);
+  expect(callback).toHaveBeenCalledTimes(1);
+  jest.advanceTimersByTime(50000);
+  expect(callback).toHaveBeenCalledTimes(101);
+});
+
+it('stops game after clicking stop button', () => {
+  const callback: any = jest.fn();
+
+  jest.useFakeTimers();
+
+  renderHook(() => useInterval(callback, 500));
+  render(<Game />);
+  // no callbacks if game not started
+  jest.advanceTimersByTime(5000);
+  expect(callback).toHaveBeenCalledTimes(0);
+
+  // start game
+  const startButton = screen.getByText('Start');
+  fireEvent.click(startButton);
+
+  jest.advanceTimersByTime(499);
+  expect(callback).toHaveBeenCalledTimes(0);
+  jest.advanceTimersByTime(1);
+  expect(callback).toHaveBeenCalledTimes(1);
+  jest.advanceTimersByTime(50000);
+  expect(callback).toHaveBeenCalledTimes(101);
+
+  // stop game
+  const stopButton = screen.getByText('Stop');
+  fireEvent.click(stopButton);
+
+  jest.advanceTimersByTime(5000);
+  expect(callback).toHaveBeenCalledTimes(101);
+});
+
+it('empties board after clicking empty button', () => {
+  render(<Game />);
+
+  const emptyButton = screen.getByText('Empty');
+  fireEvent.click(emptyButton);
+
+  const livingCells = screen.getAllByTestId('living-cell');
+  const deadCells = screen.getAllByTestId('dead-cell');
+  expect(livingCells[0]).not.toBeInTheDocument();
+  expect(deadCells[0]).toBeInTheDocument();
+});
+
+it('resets board after clicking reset button', () => {
+  render(<Game />);
+
+  const emptyButton = screen.getByText('Empty');
+  fireEvent.click(emptyButton);
+
+  const livingCells = screen.getAllByTestId('living-cell');
+  const deadCells = screen.getAllByTestId('dead-cell');
+  expect(livingCells[0]).not.toBeInTheDocument();
+  expect(deadCells[0]).toBeInTheDocument();
+
+  const resetButton = screen.getByText('Reset');
+  fireEvent.click(resetButton);
+
+  const livingCellsAfterReset = screen.getAllByTestId('living-cell');
+  const deadCellsAfterReset = screen.getAllByTestId('dead-cell');
+  expect(livingCellsAfterReset[0]).toBeInTheDocument();
+  expect(deadCellsAfterReset[0]).toBeInTheDocument();
 });
